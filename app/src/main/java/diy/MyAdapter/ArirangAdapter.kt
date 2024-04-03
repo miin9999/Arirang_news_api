6643b6216d555d4fc8190f89d21e559957f0aa7e
miin <busster@naver.com>
2024-04-03 17:33:40 +0900

    detailView까지 완성

--- app/src/main/java/diy/MyAdapter/ArirangAdapter.kt
+++ app/src/main/java/diy/MyAdapter/ArirangAdapter.kt
@@ -1,23 +1,47 @@
 package diy.MyAdapter
 
 import android.content.Intent
+import android.telecom.Call.Details
+import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import diy.NewsDetailActivity
+import diy.arirangnewsapi.R
 import diy.arirangnewsapi.arirang_models.Item
 import diy.arirangnewsapi.databinding.ItemListBinding
 
-class ArirangAdapter(val itemClicked : ()->Unit): ListAdapter<Item, ArirangAdapter.MyViewHolder>(differ){
+
+import kotlinx.coroutines.withContext
+import java.security.AccessController.getContext
+
+
+
+class ArirangAdapter(): ListAdapter<Item, ArirangAdapter.MyViewHolder>(differ){
+
+
+    interface OnItemClickListener{
+        fun onItemClick(v:View,data:Item,pos:Int)
+    }
+
+
+    private var listener : OnItemClickListener?= null
+
+    fun setOnItemClickListener(listener:OnItemClickListener){
+        this.listener = listener
+    }
 
 
     inner class MyViewHolder(val binding: ItemListBinding):RecyclerView.ViewHolder(binding.root){
 
         fun bind(itemModel: Item){
+
             binding.titleTextView.text = itemModel.title
             binding.contentTextView.text = itemModel.content
 
@@ -26,16 +50,19 @@ class ArirangAdapter(val itemClicked : ()->Unit): ListAdapter<Item, ArirangAdapt
                 .load(itemModel.thumUrl)
                 .into(binding.coverImageView)
 
-            binding.root.setOnClickListener {
-                itemClicked()
+            val pos = adapterPosition
+            Log.d("adapterPosition",pos.toString())
+
+            if(pos!= RecyclerView.NO_POSITION){
+                itemView.setOnClickListener {
+                    Log.d("who first","adapter")
+                    listener?.onItemClick(itemView,itemModel,pos)
+                }
             }
-
-
         }
-
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArirangAdapter.MyViewHolder {
 
         return MyViewHolder(ItemListBinding.inflate(LayoutInflater.from(parent.context),parent,false))
 
@@ -43,7 +70,9 @@ class ArirangAdapter(val itemClicked : ()->Unit): ListAdapter<Item, ArirangAdapt
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        Log.d("currentList onBindViewHolder",currentList.toString())
         holder.bind(currentList[position])
+
     }
 
 
