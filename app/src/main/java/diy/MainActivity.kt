62b9d30700eb609b6e71590ee9eeafa579a4c567
miin <busster@naver.com>
2024-04-03 17:33:40 +0900

    detailView까지 완성

--- app/src/main/java/diy/MainActivity.kt
+++ app/src/main/java/diy/MainActivity.kt
@@ -1,14 +1,17 @@
 package diy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import diy.MyAdapter.ArirangAdapter
 
 import diy.arirangnewsapi.Repository
+import diy.arirangnewsapi.arirang_models.Item
 import diy.arirangnewsapi.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
@@ -23,38 +26,60 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var adapter : ArirangAdapter
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
+
+        coroutineScope()
+        adapterSetting()
+
+
+    }
+
+    private fun adapterSetting(){
+
         adapter = ArirangAdapter(
-            itemClicked = {
-              val intent = Intent(this,NewsDetailActivity::class.java)
-                startActivity(intent)
+//            itemClicked = {
+//                val intent = Intent(this,NewsDetailActivity::class.java)
+//
+//                startActivity(intent)
+//            }
+        ).apply{
+            setOnItemClickListener(object: ArirangAdapter.OnItemClickListener{
+            override fun onItemClick(v: View, data: Item, pos: Int) {
+                Intent(this@MainActivity,NewsDetailActivity::class.java).apply{
+                    putExtra("putContentData",data.content)
+                    putExtra("putTitleData",data.title)
+                    putExtra("putThumbUrl",data.thumUrl)
+
+                    Log.d("fdsfefef",data.content.toString())
+                    //Log.d("who first","main")
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }.run{ startActivity(this) }
+
             }
-        )
+        })
+
+    }
 
 
         binding.recyclerViewId.layoutManager = LinearLayoutManager(this)
         binding.recyclerViewId.adapter=adapter
+    }
 
-
-        // reflection 실습
-        val kClass: KClass<ArirangAdapter> = ArirangAdapter::class
-        Log.d("reflection",kClass.functions.toString())
-
-
+    private fun coroutineScope(){
         try{
 
             scope.launch{
 
-                val dataSet = Repository.getNewsFromApiService(1, 50)
+                val dataSet = Repository.getNewsFromApiService(1, 15)
                 Log.d("DATA SET",dataSet.toString())
+
+
 
                 adapter.submitList(dataSet)
 
@@ -67,8 +92,6 @@ class MainActivity : AppCompatActivity() {
         }finally {
 
         }
-
-
-
     }
+
 }
